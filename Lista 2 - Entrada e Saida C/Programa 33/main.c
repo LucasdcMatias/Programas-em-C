cc62be3fcd829f5187a3112aa5b7d12adffda3f8
Lucas Matias <lucas_matias_3@hotmail.com>
2023-10-13 08:32:22 -0300

    Renomeação da pasta de C++ para C

---
+++ Lista 2 - Entrada e Saida C/Programa 33/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	float x, x2, x3;
+	
+	printf("Digite um valor para X: ");
+	scanf("%f", &x);
+	
+	x2 = x * x;
+	x3 = x * x * x;
+	
+	if (x <= 1)
+	printf("F(x) = 1");
+	
+	else if (x <= 2)
+	printf("F(x) = 2");
+	
+	else if (x <= 3 && x < 2)
+	printf("Fx = %f", x2);
+	
+	else if (x > 3)
+	printf("Fx = %f", x3);
+	
+	
+	return 0;
+}
